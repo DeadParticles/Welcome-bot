4b51ebb8e0bae3a9a23ec67c4c31efd4f5126003
Voltern <33236873+Voltern@users.noreply.github.com>
2017-12-10 15:38:15 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,4 @@
+bot.on('serverNewMember',function (user, userID, channelID, message, evt){
+if(user === bot.servers.get('id',"SERVERID"))
+bot.sendMessage(channelID.get('name','general'),"Welcome to my Server "+user.mention()+"!")
+})
